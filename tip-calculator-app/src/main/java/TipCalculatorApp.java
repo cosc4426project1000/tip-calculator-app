a1432cb533b5025ab6472661005baf6ba1747f0f
cosc4426project1000 <cosc4426project1000@gmail.com>
2019-05-24 08:55:30 +0700

    testing Jenkins build 2

--- tip-calculator-app/src/main/java/TipCalculatorApp.java
+++ tip-calculator-app/src/main/java/TipCalculatorApp.java
@@ -3,7 +3,7 @@ public class TipCalculatorApp {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to may Tip calculator app");
+		System.out.println("Welcome to may Tip calculator app - version 2");
 	}
 
 }
